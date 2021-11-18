b0fd188de74c1c07800e690b3e8c2c1701b2b969
파링 <parangee9706@gmail.com>
2021-11-18 20:09:55 +0900

    backup

--- src/structures/client.ts
+++ src/structures/client.ts
@@ -1,6 +1,11 @@
 import { CommandClient } from '@pikokr/command.ts'
 import Discord, { Intents, IntentsString } from 'discord.js'
 import { config } from '../config'
+import chokidar from 'chokidar'
+import * as path from 'path'
+import { KModulePath } from '@pikokr/command.ts/dist/constants'
+import chalk from 'chalk'
+import * as fs from 'fs'
 
 export class Client extends CommandClient {
     constructor() {
@@ -19,5 +24,30 @@ export class Client extends CommandClient {
         })
 
         this.registry.loadModulesIn('modules')
+
+        const watcher = chokidar.watch(require.main!.path)
+
+        watcher.on('change', async path1 => {
+            if (path1.startsWith(path.join(require.main!.path,'modules'))) {
+                try {
+                    const mod = this.registry.modules.find(x=>Reflect.getMetadata(KModulePath, x) === path1)
+                    if (!mod) {
+                        await this.registry.loadModule(path1, true)
+                    } else {
+                        await this.registry.reloadModule(mod)
+                    }
+                    console.log(`[${chalk.green('SUCCESS')}] ${chalk.blueBright(path1)} - ${chalk.green('Success')}`)
+                } catch (e: any) {
+                    console.log(`[${chalk.redBright('ERROR')}] ${chalk.blueBright(path1)} - ${chalk.redBright(e.message)}`)
+                }
+                return
+            }
+            const mod = require.cache[require.resolve(path1)]
+            if (mod) {
+                const f = (await fs.readFileSync(path1)).toString()
+                if (f.startsWith('// reloadable')) {
+                }
+            }
+        })
     }
 }
