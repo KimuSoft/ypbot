51b0229d64d4b8d559b319cb1aa678add10c5944
파링 <parangee9706@gmail.com>
2021-11-18 23:16:39 +0900

    session

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { Client } from './structures/client'
-import { config } from './config'
 
 export const cts = new Client()
 
 import { initServer } from './webManager'
 import { setGlobal } from './utils/global'
+import db from './utils/db'
 
 setGlobal('yp.client', cts.client)
+setGlobal('yp.db', db)
 
 // cts.client.login(config.token).then(() => initServer())
 
