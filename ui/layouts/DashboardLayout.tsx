94691376ccb899aad7227df2a62457559054e3bf
파링 <parangee9706@gmail.com>
2022-03-23 21:19:47 +0900

    sort guild list

--- ui/layouts/DashboardLayout.tsx
+++ ui/layouts/DashboardLayout.tsx
@@ -2,18 +2,22 @@ import React from 'react'
 import Sidebar from '../organisms/Sidebar'
 import { Guild } from '../../src/globalTypes'
 import { faker } from '@faker-js/faker'
+import _ from 'lodash'
 
 const DashboardLayout: React.FC = ({ children }) => {
     const fakeGuilds = React.useMemo(() => {
-        return new Array(10).fill(void 0).map(
-            () =>
-                ({
-                    id: faker.datatype.uuid(),
-                    name: faker.name.title(),
-                    iconURL: faker.image.avatar(),
-                    isOwner: faker.datatype.boolean(),
-                } as Guild)
-        )
+        return _.sortBy(
+            new Array(10).fill(void 0).map(
+                () =>
+                    ({
+                        id: faker.datatype.uuid(),
+                        name: faker.name.title(),
+                        iconURL: faker.image.avatar(),
+                        isOwner: faker.datatype.boolean(),
+                    } as Guild)
+            ),
+            'isOwner'
+        ).reverse()
     }, [])
 
     return (
