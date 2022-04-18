931adc3c9bfa4e556d2e311a6575e030c18aee08
파링 <parangee9706@gmail.com>
2022-04-18 20:14:34 +0900

    feat: rule not found message & create button(dummy)

--- pages/dashboard/rules/index.tsx
+++ pages/dashboard/rules/index.tsx
@@ -7,7 +7,7 @@ const Rules: NextPageWithLayout = () => {
     return (
         <div className="flex flex-col h-full">
             <div className="bg-stone-900 flex-grow p-[24px] rounded-[16px] overflow-auto">
-                <RuleList url="/rules/me" title="내 커스텀 규칙" />
+                <RuleList creatable url="/rules/me" title="내 커스텀 규칙" />
             </div>
         </div>
     )
--- ui/organisms/RuleList.tsx
+++ ui/organisms/RuleList.tsx
@@ -3,19 +3,19 @@ import RuleSkeleton from './RuleSkeleton'
 import ListCollapse from './ListCollapse'
 import { api } from '../../webUtils/api'
 import useSWR from 'swr'
+import type { Rule } from '@prisma/client'
+import { FaPlus } from 'react-icons/fa'
 
 const fetcher = (url: string) => api.get(url).then((x) => x.data)
 
-const RuleList: React.FC<{ url: string; title: React.ReactNode }> = ({ url, title }) => {
-    const { data, error } = useSWR(url, { fetcher })
-
-    console.log(data)
+const RuleList: React.FC<{ url: string; title: React.ReactNode; creatable?: boolean }> = ({ url, title, creatable }) => {
+    const { data, error } = useSWR<Rule[]>(url, { fetcher })
 
     return (
         <ListCollapse title={title}>
             {(() => {
                 if (error) {
-                    return <div className="bg-red-500 p-4 rounded-md mt-[12px]">{data?.error ?? error.message}</div>
+                    return <div className="bg-red-500 p-4 rounded-md mt-[12px]">{(data as any)?.error ?? error.message}</div>
                 }
                 if (!data) {
                     return (
@@ -27,7 +27,22 @@ const RuleList: React.FC<{ url: string; title: React.ReactNode }> = ({ url, titl
                     )
                 }
                 if (data) {
-                    return <div>data</div>
+                    if (!data.length && !creatable) {
+                        return (
+                            <div className="mt-[12px] flex flex-col items-center">
+                                <div className="text-xl">규칙이 없습니다</div>
+                            </div>
+                        )
+                    }
+                    return (
+                        <div className="mt-[12px] grid gap-[12px] grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                            {creatable && (
+                                <div className="flex min-h-[64px] bg-stone-800 justify-center items-center rounded-[16px] hover:bg-stone-700 transition-all select-none cursor-pointer">
+                                    <FaPlus size={24} />
+                                </div>
+                            )}
+                        </div>
+                    )
                 }
             })()}
         </ListCollapse>
