924a5d27082ed8d17d433295f43c591b1f0110c6
pikokr <parangee9706@gmail.com>
2022-05-15 10:22:27 +0900

    fix checkbox defaultchecked

--- app/components/app/forms/ValidatedCheckbox.tsx
+++ app/components/app/forms/ValidatedCheckbox.tsx
@@ -2,13 +2,21 @@ import type { CheckboxProps } from '@mui/material'
 import { Checkbox } from '@mui/material'
 import { useTransition } from '@remix-run/react'
 import React from 'react'
+import { useField } from 'remix-validated-form'
 
 export const ValidatedCheckbox: React.FC<CheckboxProps & { name: string }> = ({
   name,
   ...props
 }) => {
+  const field = useField(name)
+
   const submitting = useTransition().state === 'submitting'
   return (
-    <Checkbox name={name} {...props} disabled={props.disabled || submitting} />
+    <Checkbox
+      name={name}
+      defaultChecked={field.defaultValue}
+      {...props}
+      disabled={props.disabled || submitting}
+    />
   )
 }
