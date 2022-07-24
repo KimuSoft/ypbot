1864035fcf63678b307d2f1b2d1cb3c289edf066
pikokr <parangee9706@gmail.com>
2022-07-24 16:13:48 +0900

    fix: button size

--- frontend/src/components/atoms/Button.svelte
+++ frontend/src/components/atoms/Button.svelte
@@ -4,7 +4,7 @@
 
 <div
   on:click
-  class="inline-block px-4 py-2 rounded-xl select-none font-extrabold text-xl hover:brightness-90 active:brightness-75 transition-all cursor-pointer {className}"
+  class="inline-block px-4 py-[4px] rounded-full select-none font-extrabold text-md hover:brightness-90 active:brightness-75 transition-all cursor-pointer {className}"
 >
   <slot />
 </div>
