42e31d5a53b5fb1aeaa946274798640f318d610e
pikokr <parangee9706@gmail.com>
2022-07-25 13:42:15 +0900

    feat: sentry environment

--- frontend/src/utils/sentry.ts
+++ frontend/src/utils/sentry.ts
@@ -1,3 +1,4 @@
+import { dev } from '$app/env'
 import * as Sentry from '@sentry/browser'
 import { Integrations } from '@sentry/tracing'
 
@@ -5,4 +6,5 @@ Sentry.init({
   dsn: 'https://0e689abf305748e59d1d5e1a6861f578@sentry.oci.pikokr.dev/5',
   integrations: [new Integrations.BrowserTracing()],
   tracesSampleRate: 1.0,
+  environment: dev ? 'dev' : 'production',
 })
