aaeb78277ac41fc4320c784f4163ec5a794c730b
pikokr <parangee9706@gmail.com>
2022-07-25 10:02:59 +0900

    feat: save rule meta

--- frontend/src/routes/app/rules/[id]/index.svelte
+++ frontend/src/routes/app/rules/[id]/index.svelte
@@ -11,6 +11,8 @@
   import RuleAddDialog from '@/components/organisms/RuleAddDialog.svelte'
   import { getApollo } from '@/utils/apollo'
   import { gql } from '@apollo/client/core'
+  import { AlertSeverity, enqueueAlert } from '@/utils/alert'
+  import { argsToArgsConfig } from 'graphql/type/definition'
 
   const ruleContext =
     getContext<
@@ -23,6 +25,87 @@
 
   const onSubmit = async (e: SubmitEvent) => {
     e.preventDefault()
+
+    const { data } = await getApollo().mutate<{
+      rule?: {
+        updateMeta?: Rule & {
+          references: (Rule & { author: YPUser; counts: RuleCounts })[]
+        }
+      }
+    }>({
+      mutation: gql`
+        mutation Mutation(
+          $ruleId: String!
+          $name: String
+          $description: String
+        ) {
+          rule(id: $ruleId) {
+            updateMeta(name: $name, description: $description) {
+              id
+              name
+              description
+              elements {
+                id
+                name
+                regex
+                ruleType
+              }
+              references {
+                id
+                name
+                description
+                counts {
+                  black
+                  white
+                  include
+                }
+                shareCode
+                sharingEnabled
+                author {
+                  id
+                  tag
+                  avatar
+                }
+                isOfficial
+              }
+              counts {
+                black
+                white
+                include
+              }
+              shareCode
+              sharingEnabled
+              isOfficial
+            }
+          }
+        }
+      `,
+      variables: {
+        ruleId: rule.id,
+        name: rule.name,
+        description: rule.description,
+      },
+    })
+
+    try {
+      if (data?.rule?.updateMeta) {
+        rule = data.rule.updateMeta
+        ruleContext.set(data.rule.updateMeta)
+        enqueueAlert({
+          title: '저장되었습니다.',
+          severity: AlertSeverity.Success,
+          time: 5000,
+        })
+      } else {
+        await Promise.reject()
+      }
+    } catch (e) {
+      enqueueAlert({
+        title: '저장 실패',
+        severity: AlertSeverity.Error,
+        time: 5000,
+      })
+    }
   }
 
   const onSelect = async (e: CustomEvent<{ id: string }>) => {
@@ -122,33 +205,60 @@
   </div>
 {/if}
 
-<form on:submit={onSubmit} class="max-w-[600px] mx-auto">
-  <div class="text-3xl font-bold">{rule.name}</div>
+<div class="text-3xl font-bold">{rule.name}</div>
 
-  <label class="flex flex-col mt-4">
-    <div class="text-lg pl-2">이름</div>
-    <input
-      bind:value={rule.name}
-      required
-      type="text"
-      class="bg-transparent mt-1 ring-1 rounded-full text-lg outline-none ring-white/20 focus:ring-blue-500 transition-all py-1 px-4"
-    />
-  </label>
+<div class="grid md:grid-cols-2 gap-4">
+  <form on:submit={onSubmit}>
+    <label class="flex flex-col mt-4">
+      <div class="text-lg pl-2">이름</div>
+      <input
+        bind:value={rule.name}
+        required
+        type="text"
+        class="bg-transparent mt-1 ring-1 rounded-full text-lg outline-none ring-white/20 focus:ring-blue-500 transition-all py-1 px-4"
+      />
+    </label>
 
-  <label class="flex flex-col mt-4">
-    <div class="text-lg pl-2">설명</div>
-    <input
-      bind:value={rule.description}
-      required
-      type="text"
-      class="bg-transparent mt-1 ring-1 rounded-full text-lg outline-none ring-white/20 focus:ring-blue-500 transition-all py-1 px-4"
-    />
-  </label>
+    <label class="flex flex-col mt-4">
+      <div class="text-lg pl-2">설명</div>
+      <input
+        bind:value={rule.description}
+        required
+        type="text"
+        class="bg-transparent mt-1 ring-1 rounded-full text-lg outline-none ring-white/20 focus:ring-blue-500 transition-all py-1 px-4"
+      />
+    </label>
 
-  <Button type="submit" class="bg-blue-500 w-full text-center mt-4 py-2">
-    저장하기
-  </Button>
-</form>
+    <Button type="submit" class="bg-blue-500 w-full text-center mt-4 py-2">
+      저장하기
+    </Button>
+  </form>
+  <div class="flex flex-col">
+    <label class="flex flex-col mt-4">
+      <div class="text-lg pl-2">공유 코드</div>
+      <input
+        value={rule.shareCode}
+        readonly
+        required
+        type="text"
+        class="bg-transparent mt-1 ring-1 rounded-full text-lg outline-none ring-white/20 focus:ring-blue-500 transition-all py-1 px-4"
+      />
+    </label>
+    <label class="flex flex-col mt-4">
+      <div class="text-lg pl-2">공유 링크</div>
+      <input
+        value={`${window.location.protocol}//${window.location.host}/app/rules/share?code=${rule.shareCode}`}
+        readonly
+        required
+        type="text"
+        class="bg-transparent mt-1 ring-1 rounded-full text-lg outline-none ring-white/20 focus:ring-blue-500 transition-all py-1 px-4"
+      />
+    </label>
+    <Button class="bg-blue-500 w-full text-center mt-4 py-2">
+      공유 {rule.sharingEnabled ? '비활성화' : '활성화'}
+    </Button>
+  </div>
+</div>
 
 <div class="mt-8">
   <div class="text-3xl font-bold">태그 포함</div>
