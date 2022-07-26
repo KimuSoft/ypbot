aba9cbbff959f9f92b29ca51dba724ea590e39bc
pikokr <parangee9706@gmail.com>
2022-07-26 18:01:18 +0900

    chore: remove includedRuleId

---
+++ shared/prisma/migrations/20220726085616_remove_included_rule_id/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `includedRuleId` on the `RuleElement` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "RuleElement" DROP COLUMN "includedRuleId";
