cde4e6a717bb9dabbf458ed19397a97d57bf1871
Kimu-Nowchira <hon20ke@naver.com>
2022-07-26 20:38:21 +0900

    feat: highlight is awsome

--- bot/src/modules/censor.ts
+++ bot/src/modules/censor.ts
@@ -1,12 +1,23 @@
 import { Extension, listener } from "@pikokr/command.ts"
-import { codeBlock, EmbedBuilder, Message } from "discord.js"
+import { codeBlock, Colors, EmbedBuilder, Message } from "discord.js"
 import hangul from "hangul-js"
 import { prisma, RuleType } from "shared"
+import chalk from "chalk"
 
 class CensorModule extends Extension {
+  @listener({ event: "messageUpdate" })
+  async messageUpdate(oldMsg: Message, newMsg: Message) {
+    return this.messageCreate(newMsg)
+  }
+
   @listener({ event: "messageCreate" })
   async messageCreate(msg: Message) {
     if (msg.author.bot || msg.author.id === this.client.user?.id) return
+
+    const originalContent = msg.content
+      .normalize()
+      .replace(/[!?@#$%^&*():;+-=~{}<>_\[\]|\\"',.\/`₩\s\t]/g, "")
+
     const matches = await prisma.$queryRaw<
       {
         separate: boolean
@@ -26,8 +37,8 @@ class CensorModule extends Extension {
             el."ruleType",
             el."ruleId",
             case el."separate"
-              when TRUE then ${hangul.disassembleToString(msg.content)}
-              else ${msg.content}
+              when TRUE then ${hangul.disassembleToString(originalContent)}
+              else ${originalContent}
             end as  "_Input"
           from   "RuleElement" as el)
         select el.*, el."name" as "ruleName"
@@ -51,23 +62,65 @@ class CensorModule extends Extension {
 
     const regex = new RegExp(rule.regex, "g")
 
-    const regexMatches = msg.content.matchAll(regex)
+    const normalizedContent = msg.content.normalize()
 
-    let content = msg.content
+    const regexMatches = normalizedContent.matchAll(regex)
 
-    let indexToAdd = 0
+    let content = ""
+
+    let lastIndex = 0
+
+    // function that remove symbols from the string and korean is not symbol
+    // const removeSymbols = (str: string) => {
+    //   return str
+    //     .replace(
+    //       /[^\uAC00-\uD7AF\u1100-\u11FF\u3130-\u318F\uA960-\uA97F\uD7B0-\uD7FF\u0020]/g,
+    //       ""
+    //     )
+    //     .replace(
+    //       /[\uAC00-\uD7AF\u1100-\u11FF\u3130-\u318F\uA960-\uA97F\uD7B0-\uD7FF]/g,
+    //       ""
+    //     )
+    // }
 
     for (const match of regexMatches) {
-      if (!match.index) continue
-      let adjustedIndex = match.index + indexToAdd
-      content =
-        content.slice(0, adjustedIndex) +
-        "**" +
-        content.slice(adjustedIndex, adjustedIndex + match.length) +
-        "**" +
-        content.slice(adjustedIndex, match.length)
-      indexToAdd += 4
+      if (match.index === undefined || match.input === undefined) return
+
+      content += normalizedContent.slice(lastIndex, match.index)
+
+      content += `\u001b[31m${match[0]}\u001b[0m`
+
+      lastIndex = match.index + match[0].length
     }
+
+    content += normalizedContent.slice(lastIndex)
+
+    if (content === normalizedContent) {
+      let newContent: string = ""
+      const regexMatches2 = originalContent.matchAll(regex)
+      for (const match of regexMatches2) {
+        if (match.index === undefined || match.input === undefined) return
+
+        newContent += originalContent.slice(lastIndex, match.index)
+
+        newContent += `\u001b[31m${match[0]}\u001b[0m`
+
+        lastIndex = match.index + match[0].length
+      }
+
+      if (originalContent !== content) {
+        newContent += originalContent.slice(lastIndex)
+        content += `\n-----------------------------------------------\n→ ${newContent}`
+      }
+    }
+
+    // 키뮤식 구현
+    // const matchContent = msg.content.match(regex)
+    // if (matchContent && matchContent.length) {
+    //   const regexKimu = new RegExp(`(${rule.regex})`, "g")
+    //   content = content.replace(regexKimu, "**$1**")
+    //   content = content.replace(/\*{4}/g, "")
+    // }
 
     // 알림 메시지 생성
     // const firstMatchRule = matches[0]
@@ -85,11 +138,13 @@ class CensorModule extends Extension {
       .setTimestamp()
 
     alertEmbed.setDescription(codeBlock("ansi", content))
-    alertEmbed.setFooter({
-      text: `\`${rule.ruleName}\`을(를) ${
-        rule.ruleType === "Black" ? "말했습니다." : "말하지 않았습니다."
-      }`,
-    })
+    alertEmbed
+      .setFooter({
+        text: `\`${rule.ruleName}\`을(를) ${
+          rule.ruleType === "Black" ? "말했습니다." : "말하지 않았습니다."
+        }`,
+      })
+      .setColor(Colors.Red)
 
     // 해당 서버의 알림 채널 정보 불러옴
     // const channelData = await prisma.$queryRaw<
@@ -102,7 +157,7 @@ class CensorModule extends Extension {
 
     await msg.channel.send(
       {
-        content: `${msg.author}님께서 ${msg.channel}에서 \`${rule.ruleName}\` 규칙을 위반하셨습니다.`,
+        content: `${msg.author}님이 ${msg.channel}에서 \`${rule.ruleName}\` 규칙을 위반하셨습니다.`,
         embeds: [alertEmbed],
       }
       // codeBlock("diff", matches.map((x) => `- ${x.ruleName}`).join("\n"))
