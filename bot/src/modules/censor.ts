0f7154428c3a8f087d368dcc9dcf482358671232
pikokr <parangee9706@gmail.com>
2022-07-28 10:20:46 +0900

    chore: add try catch on event

--- bot/src/modules/censor.ts
+++ bot/src/modules/censor.ts
@@ -29,6 +29,7 @@ import { YPClient } from "../structures/YPClient"
 import fs from "fs"
 import path from "path"
 import { sqlDir } from "../utils"
+import { captureException } from "@sentry/node"
 
 class CensorModule extends Extension {
   @listener({ event: "messageUpdate" })
@@ -198,140 +199,144 @@ class CensorModule extends Extension {
 
   @listener({ event: "messageCreate" })
   async messageCreate(msg: Message) {
-    if (msg.author.bot || msg.author.id === this.client.user?.id) return
-    if (!msg.content) return
-    if (!msg.guild) return
+    try {
+      if (msg.author.bot || msg.author.id === this.client.user?.id) return
+      if (!msg.content) return
+      if (!msg.guild) return
 
-    const originalContent = msg.content
-      .normalize()
-      .replace(/[!?@#$%^&*():;+-=~{}<>_\[\]|\\"',.\/`₩\s\t]/g, "")
+      const originalContent = msg.content
+        .normalize()
+        .replace(/[!?@#$%^&*():;+-=~{}<>_\[\]|\\"',.\/`₩\s\t]/g, "")
 
-    const matches = await prisma.$queryRawUnsafe<
-      {
-        separate: boolean
-        id: string
-        regex: string
-        name: string
-        ruleType: RuleType
-        ruleId: string
-        ruleName: string
-      }[]
-    >(
-      this.findRuleSql,
-      msg.channel.id,
-      msg.guild.id,
-      hangul.disassembleToString(originalContent),
-      originalContent
-    )
+      const matches = await prisma.$queryRawUnsafe<
+        {
+          separate: boolean
+          id: string
+          regex: string
+          name: string
+          ruleType: RuleType
+          ruleId: string
+          ruleName: string
+        }[]
+      >(
+        this.findRuleSql,
+        msg.channel.id,
+        msg.guild.id,
+        hangul.disassembleToString(originalContent),
+        originalContent
+      )
 
-    if (!matches.length) return
-    if (!msg.deletable) return
+      if (!matches.length) return
+      if (!msg.deletable) return
 
-    // 규칙을 위반한 메시지 삭제
-    await msg.delete()
+      // 규칙을 위반한 메시지 삭제
+      await msg.delete()
 
-    const rule = matches[0]
+      const rule = matches[0]
 
-    const regex = new RegExp(rule.regex, "g")
+      const regex = new RegExp(rule.regex, "g")
 
-    const normalizedContent = msg.content.normalize()
+      const normalizedContent = msg.content.normalize()
 
-    const regexMatches = normalizedContent.matchAll(regex)
+      const regexMatches = normalizedContent.matchAll(regex)
 
-    let content = ""
+      let content = ""
 
-    let lastIndex = 0
+      let lastIndex = 0
 
-    // function that remove symbols from the string and korean is not symbol
-    // const removeSymbols = (str: string) => {
-    //   return str
-    //     .replace(
-    //       /[^\uAC00-\uD7AF\u1100-\u11FF\u3130-\u318F\uA960-\uA97F\uD7B0-\uD7FF\u0020]/g,
-    //       ""
-    //     )
-    //     .replace(
-    //       /[\uAC00-\uD7AF\u1100-\u11FF\u3130-\u318F\uA960-\uA97F\uD7B0-\uD7FF]/g,
-    //       ""
-    //     )
-    // }
+      // function that remove symbols from the string and korean is not symbol
+      // const removeSymbols = (str: string) => {
+      //   return str
+      //     .replace(
+      //       /[^\uAC00-\uD7AF\u1100-\u11FF\u3130-\u318F\uA960-\uA97F\uD7B0-\uD7FF\u0020]/g,
+      //       ""
+      //     )
+      //     .replace(
+      //       /[\uAC00-\uD7AF\u1100-\u11FF\u3130-\u318F\uA960-\uA97F\uD7B0-\uD7FF]/g,
+      //       ""
+      //     )
+      // }
 
-    for (const match of regexMatches) {
-      if (match.index === undefined || match.input === undefined) return
-
-      content += normalizedContent.slice(lastIndex, match.index)
-
-      content += `\u001b[31m${match[0]}\u001b[0m`
-
-      lastIndex = match.index + match[0].length
-    }
-
-    content += normalizedContent.slice(lastIndex)
-
-    if (content === normalizedContent && rule.ruleType === "Black") {
-      let newContent: string = ""
-      const regexMatches2 = originalContent.matchAll(regex)
-      for (const match of regexMatches2) {
+      for (const match of regexMatches) {
         if (match.index === undefined || match.input === undefined) return
 
-        newContent += originalContent.slice(lastIndex, match.index)
+        content += normalizedContent.slice(lastIndex, match.index)
 
-        newContent += `\u001b[31m${match[0]}\u001b[0m`
+        content += `\u001b[31m${match[0]}\u001b[0m`
 
         lastIndex = match.index + match[0].length
       }
 
-      if (originalContent !== newContent) {
-        newContent += originalContent.slice(lastIndex)
-        content += `\n-----------------------------------------------\n→ ${newContent}`
+      content += normalizedContent.slice(lastIndex)
+
+      if (content === normalizedContent && rule.ruleType === "Black") {
+        let newContent: string = ""
+        const regexMatches2 = originalContent.matchAll(regex)
+        for (const match of regexMatches2) {
+          if (match.index === undefined || match.input === undefined) return
+
+          newContent += originalContent.slice(lastIndex, match.index)
+
+          newContent += `\u001b[31m${match[0]}\u001b[0m`
+
+          lastIndex = match.index + match[0].length
+        }
+
+        if (originalContent !== newContent) {
+          newContent += originalContent.slice(lastIndex)
+          content += `\n-----------------------------------------------\n→ ${newContent}`
+        }
       }
+
+      // 키뮤식 구현
+      // const matchContent = msg.content.match(regex)
+      // if (matchContent && matchContent.length) {
+      //   const regexKimu = new RegExp(`(${rule.regex})`, "g")
+      //   content = content.replace(regexKimu, "**$1**")
+      //   content = content.replace(/\*{4}/g, "")
+      // }
+
+      // 알림 메시지 생성
+      // const firstMatchRule = matches[0]
+      // const matchStrings = msg.content.match(firstMatchRule.regex)
+      // let matchStr: string | null = null
+      // if (matchStrings && matchStrings.length) {
+      //   matchStr = matchStrings[0]
+      // }
+
+      const alertEmbed = new EmbedBuilder()
+        .setAuthor({
+          iconURL: msg.author.displayAvatarURL(),
+          name: msg.author.tag,
+        })
+        .setTimestamp()
+
+      alertEmbed.setDescription(codeBlock("ansi", content))
+      alertEmbed
+        .setFooter({
+          text: `\`${rule.name}\`을(를) ${
+            rule.ruleType === "Black" ? "말했습니다." : "말하지 않았습니다."
+          }`,
+        })
+        .setColor(Colors.Red)
+
+      const g = await prisma.guild.findUnique({
+        where: { id: msg.guild.id },
+        select: { alertChannelId: true },
+      })
+
+      const ch =
+        (msg.guild.channels.cache.get(
+          g?.alertChannelId as string
+        ) as TextBasedChannel) ?? msg.channel
+
+      await ch.send({
+        content: `${msg.author}님이 ${msg.channel}에서 \`${rule.ruleName}\` 규칙을 위반하셨습니다.`,
+        embeds: [alertEmbed],
+      })
+    } catch (e) {
+      captureException(e)
     }
-
-    // 키뮤식 구현
-    // const matchContent = msg.content.match(regex)
-    // if (matchContent && matchContent.length) {
-    //   const regexKimu = new RegExp(`(${rule.regex})`, "g")
-    //   content = content.replace(regexKimu, "**$1**")
-    //   content = content.replace(/\*{4}/g, "")
-    // }
-
-    // 알림 메시지 생성
-    // const firstMatchRule = matches[0]
-    // const matchStrings = msg.content.match(firstMatchRule.regex)
-    // let matchStr: string | null = null
-    // if (matchStrings && matchStrings.length) {
-    //   matchStr = matchStrings[0]
-    // }
-
-    const alertEmbed = new EmbedBuilder()
-      .setAuthor({
-        iconURL: msg.author.displayAvatarURL(),
-        name: msg.author.tag,
-      })
-      .setTimestamp()
-
-    alertEmbed.setDescription(codeBlock("ansi", content))
-    alertEmbed
-      .setFooter({
-        text: `\`${rule.name}\`을(를) ${
-          rule.ruleType === "Black" ? "말했습니다." : "말하지 않았습니다."
-        }`,
-      })
-      .setColor(Colors.Red)
-
-    const g = await prisma.guild.findUnique({
-      where: { id: msg.guild.id },
-      select: { alertChannelId: true },
-    })
-
-    const ch =
-      (msg.guild.channels.cache.get(
-        g?.alertChannelId as string
-      ) as TextBasedChannel) ?? msg.channel
-
-    await ch.send({
-      content: `${msg.author}님이 ${msg.channel}에서 \`${rule.ruleName}\` 규칙을 위반하셨습니다.`,
-      embeds: [alertEmbed],
-    })
   }
 }
 
