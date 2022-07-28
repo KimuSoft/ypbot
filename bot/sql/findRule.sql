b6f819b1a89be41326d6b1c98fb19fea52035594
pikokr <parangee9706@gmail.com>
2022-07-28 10:06:18 +0900

    fix: sql

--- bot/sql/findRule.sql
+++ bot/sql/findRule.sql
@@ -52,25 +52,21 @@ _elements as (
       from "_second" as _s
       where _s."id" = r."ruleId"
     )
-),
-_result as (
-  select "id",
-    "name",
-    "ruleType",
-    "regex",
-    "separate"
-  from _elements as r
-  where (
-      (
-        r."ruleType" = 'White'::"RuleType"
-        and r."regex" ~* "_Keyword"
-      )
-      or (
-        r."ruleType" = 'Black'::"RuleType"
-        and not r."regex" ~* "_Keyword"
-      )
-    )
-  limit 1
 )
-select *
-from _result
+select "id",
+  "name",
+  "ruleType",
+  "regex",
+  "separate"
+from _elements as r
+where (
+    (
+      r."ruleType" = 'White'::"RuleType"
+      and r."regex" ~* "_Keyword"
+    )
+    or (
+      r."ruleType" = 'Black'::"RuleType"
+      and not r."regex" ~* "_Keyword"
+    )
+  )
+limit 1
