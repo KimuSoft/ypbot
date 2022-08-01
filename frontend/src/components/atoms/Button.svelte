3a7acbadf5d19e6f799dfd4a92932ada59a80c85
pikokr <parangee9706@gmail.com>
2022-08-01 12:43:22 +0900

    fix: button

--- frontend/src/components/atoms/Button.svelte
+++ frontend/src/components/atoms/Button.svelte
@@ -8,8 +8,10 @@
   on:click
   {...$$restProps}
   class="inline-block px-4 py-[4px] whitespace-nowrap {className.includes(
-    'rounded' ? '' : 'rounded-full'
-  )} select-none font-extrabold text-md hover:brightness-90 active:brightness-75 transition-all cursor-pointer {className}"
+    'rounded'
+  )
+    ? ''
+    : 'rounded-full'} select-none font-extrabold text-md hover:brightness-90 active:brightness-75 transition-all cursor-pointer {className}"
 >
   <slot />
 </button>
