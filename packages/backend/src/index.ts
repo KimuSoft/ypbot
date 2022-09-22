cc0b450ed826e4d72ee9bbb8a09d96bde27994be
pikokr <parangee9706@gmail.com>
2022-09-22 13:41:44 +0900

    feat: basic web server

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello, world!')
+import chalk from 'chalk'
+import fastify from 'fastify'
+
+const server = fastify()
+
+const addr = await server.listen({ host: '0.0.0.0', port: 3000 })
+
+console.log(chalk.blue(`Listning on ${chalk.green(addr)}`))
