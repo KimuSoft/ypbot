26ec9d1fc399f308e3388b1f38754e7853109380
pikokr <parangee9706@gmail.com>
2022-09-23 18:44:59 +0900

    chore: export RuleElement

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -2,17 +2,18 @@ import { MikroORM } from '@mikro-orm/core'
 
 import { dbUrl } from './constants.js'
 import { Rule } from './entities/Rule.js'
+import { RuleElement } from './entities/RuleElement.js'
 import { User } from './entities/User.js'
 import { Visibility } from './enums/Visibility.js'
 
 export { UserFlags } from './flags/UserFlags.js'
 
 export const orm = await MikroORM.init({
-  entities: [User, Rule],
+  entities: [User, Rule, RuleElement],
   clientUrl: dbUrl,
   type: 'postgresql',
 })
 
-export const YPEntities = [User, Rule]
+export const YPEntities = [User, Rule, RuleElement]
 
-export { User, Visibility, Rule }
+export { User, Visibility, Rule, RuleElement }
