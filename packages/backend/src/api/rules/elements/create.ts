9368160abc754aa456b9e4aaf136fabc9d140a7b
pikokr <parangee9706@gmail.com>
2022-09-23 18:49:23 +0900

    feat: create rule element

---
+++ packages/backend/src/api/rules/elements/create.ts
@@ -0,0 +1,29 @@
+import { Static, Type } from '@sinclair/typebox'
+import { RuleElement } from '@ypbot/database'
+import { FastifyPluginAsync } from 'fastify'
+
+const CreateElementSchema = Type.Object({
+  name: Type.String({ minLength: 1 }),
+  keyword: Type.String({ minLength: 1 }),
+  advanced: Type.Boolean(),
+})
+
+export const createRuleElement: FastifyPluginAsync = async (server) => {
+  server.post<{
+    Body: Static<typeof CreateElementSchema>
+  }>('/', { schema: { body: CreateElementSchema } }, async (req) => {
+    const { name, keyword, advanced } = req.body
+    const rule = req.context.apiRule
+
+    const elem = new RuleElement()
+
+    elem.name = name
+    elem.keyword = keyword
+    elem.advanced = advanced
+    elem.rule = rule
+
+    await req.em.persistAndFlush(elem)
+
+    return elem
+  })
+}
