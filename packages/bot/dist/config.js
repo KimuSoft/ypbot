14050d6f04bbb910dae4649eecc810bd4d4a7aab
pikokr <parangee9706@gmail.com>
2022-09-25 18:59:22 +0900

    chore: remove dist directory

--- packages/bot/dist/config.js
+++
@@ -1,5 +0,0 @@
-import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
-const dirname = fileURLToPath(import.meta.url);
-dotenv.config({ path: path.join(dirname, '..', '..', '..', '..', '.env') });
--- packages/bot/dist/utlis/rpc.js
+++
@@ -1,2 +0,0 @@
-import io from 'socket.io-client';
-export const rpc = io(process.env.RPC_URL, { autoConnect: false });
